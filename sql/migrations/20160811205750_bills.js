da260a3c371a9bd3d7dd6d24872d65b6983a39a4
Johannes Kissel <johannes.kissel@disy.net>
2016-08-11 23:48:50 +0200

    changed id to be text instead of autoincrement

--- sql/migrations/20160811205750_bills.js
+++ sql/migrations/20160811205750_bills.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('bills', function (table) {
-    table.increments()
+    table.text('id').primary()
     table.dateTime('date_created').defaultTo(knex.fn.now())
     table.dateTime('date_paid')
     table.text('customer')
