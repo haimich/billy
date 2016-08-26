cc477b253693ce6d1177d68df35ab06602a04e7f
Michael Mueller <michael.mueller@1und1.de>
2016-08-26 14:21:02 +0200

    merged config

--- conf/development.js
+++ conf/development.js
@@ -3,6 +3,14 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: "./bills.sqlite"
-    }
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: './sql/migrations'
+    },
+    seeds: {
+      directory: './sql/seeds'
+    },
+    useNullAsDefault: true // see http://knexjs.org/#Builder-insert
   }
 }
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,2 @@
-module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: "./bills.sqlite"
-  },
-  migrations: {
-    tableName: 'migrations',
-	directory: './sql/migrations'
-  },
-  seeds: {
-    directory: './sql/seeds'
-  },
-  useNullAsDefault: true // see http://knexjs.org/#Builder-insert
-};
+const devConfig = require('./conf/development')
+module.exports = devConfig.knex
