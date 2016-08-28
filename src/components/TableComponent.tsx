97e51149aa89d9abdfa5cfdc6cd8fbcb93099b0c
Michael Müller <haimich@gmx.net>
2016-08-28 18:53:08 +0200

    reordered stuff

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -16,24 +16,29 @@ export default class TableComponent extends React.Component<any, {}> {
   }
 
   render() {
-    const cellEditOptions = {
-      mode: this.getEditMode(),
-      blurToSave: true,
-      afterSaveCell: this.onAfterSaveCell,
-    }
 
     return (
       <div id="table-container">
+        <BootstrapTable 
+          data={this.props.bills}
+          striped={true}
+          cellEdit={{
+            mode: this.getEditMode(),
+            blurToSave: true,
+            afterSaveCell: this.onAfterSaveCell,
+          }}
+          hover={true}
+          search={true}
+          pagination={false}>
 
-        <BootstrapTable data={this.props.bills} striped={true} cellEdit={cellEditOptions} hover={true} search={true} pagination={false}>
           <TableHeaderColumn isKey={true} dataField="id" width="140" dataSort={true}>Rechnungsnr.</TableHeaderColumn>
           <TableHeaderColumn dataField="customer" width="300" dataSort={true}>Kunde</TableHeaderColumn>
           <TableHeaderColumn dataField="amount" width="90" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>Betrag</TableHeaderColumn>
           <TableHeaderColumn dataField="date_created" width="170" dataFormat={dateFormatter} dataSort={true}>Rechnungsdatum</TableHeaderColumn>
           <TableHeaderColumn dataField="date_paid" width="190" dataFormat={dateFormatter} dataSort={true}>Zahlung erhalten am</TableHeaderColumn>
           <TableHeaderColumn dataField="comment" width="400" dataSort={true}>Kommentar</TableHeaderColumn>
-        </BootstrapTable>
 
+        </BootstrapTable>
       </div>
     )
 
