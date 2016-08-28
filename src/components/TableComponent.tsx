d583c044191abea290fd809790e6721504b42e17
Michael Müller <haimich@gmx.net>
2016-08-28 18:18:32 +0200

    added inplace editing

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, CellEditClickMode } from 'react-bootstrap-table';
 import Bill from '../models/BillModel';
 import { dateFormatter, currencyFormatter } from '../helpers/formatters';
 
 export default class TableComponent extends React.Component<any, {}> {
 
+  getEditMode(): CellEditClickMode {
+    return 'click'
+  }
+
+  onAfterSaveCell(row, cellName, cellValue) {
+    console.log("Save cell '" + cellName + "' with value '" + cellValue+"'")
+    console.log("The whole row :", row)
+  }
+
   render() {
+    const cellEditOptions = {
+      mode: this.getEditMode(),
+      blurToSave: true,
+      afterSaveCell: this.onAfterSaveCell,
+    }
 
     return (
       <div id="table-container">
 
-        <BootstrapTable data={this.props.bills} striped={true} hover={true} search={true} pagination={false}>
+        <BootstrapTable data={this.props.bills} striped={true} cellEdit={cellEditOptions} hover={true} search={true} pagination={false}>
           <TableHeaderColumn isKey={true} dataField="id" width="140" dataSort={true}>Rechnungsnr.</TableHeaderColumn>
           <TableHeaderColumn dataField="customer" width="300" dataSort={true}>Kunde</TableHeaderColumn>
           <TableHeaderColumn dataField="amount" width="90" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>Betrag</TableHeaderColumn>
