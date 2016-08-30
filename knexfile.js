11a40b4f5eda7cb27599ba451cab71e1e3eebe14
Michael Mueller <michael.mueller@1und1.de>
2016-08-30 13:49:59 +0200

    copied config due to usage of electron-settings

--- knexfile.js
+++ knexfile.js
@@ -1,2 +1,14 @@
-const devConfig = require('./conf/development')
-module.exports = devConfig.knex
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: "./bills.sqlite"
+  },
+  migrations: {
+    tableName: 'migrations',
+    directory: './sql/migrations'
+  },
+  seeds: {
+    directory: './sql/seeds'
+  },
+  useNullAsDefault: true // see http://knexjs.org/#Builder-insert
+}
