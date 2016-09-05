f6e80153c0de12f7b61508fe21440ead46540392
Michael Mueller <michael.mueller@1und1.de>
2016-09-05 09:50:35 +0200

    minior tweaking

--- src/common/windows.ts
+++ src/common/windows.ts
@@ -32,7 +32,7 @@ export function openMainWindow() {
 }
 
 export function openImportWindow() {
-  importWindow = new BrowserWindow({ width: 370, height: 250 })
+  importWindow = new BrowserWindow({ width: 380, height: 260 })
   importWindow.loadURL(`file://${getAppFolder()}/import.html`)
 
   ipcMain.on('import-finished', () => {
