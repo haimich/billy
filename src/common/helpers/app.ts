4a08a8dda8c781f5eb8ba3aea1b0e2061fde6dd5
Michael Müller <haimich@gmx.net>
2016-09-26 23:40:11 +0200

    fixed isDev

--- src/common/helpers/app.ts
+++ src/common/helpers/app.ts
@@ -1,8 +1,7 @@
 import * as path from 'path'
+import { env } from 'process'
 
-export function isDev(): boolean {
-  return require('electron-is-dev')
-}
+export const isDev: boolean = (env['NODE_ENV'] === 'development')
 
 export function getAppFolder(): string {
   return path.join(__dirname, '../../../')
