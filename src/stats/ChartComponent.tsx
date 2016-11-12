d5b38736b4a1af3587f6f1b4fcbb174ab3b449e1
Michael Müller <haimich@gmx.net>
2016-11-12 10:42:28 +0100

    added basic charts component

--- src/stats/ChartComponent.tsx
+++ src/stats/ChartComponent.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+const chartjs = require('chart.js')
 
 export default class ChartComponent extends React.Component<any, {}> {
+
+  constructor(props) {
+    super(props)
+
+    console.log(chartjs)
+  }
 
   render() {
     return (
