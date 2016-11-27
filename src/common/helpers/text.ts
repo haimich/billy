967e23bfaaec2d66bb750e8d9d48a3ad320924ea
Michael Mueller <haimich@gmx.net>
2016-11-27 11:26:43 +0100

    implemented 'Mac shortening'

--- src/common/helpers/text.ts
+++ src/common/helpers/text.ts
@@ -1,5 +1,8 @@
 import * as path from 'path' 
 
+const MIN_PREFIX = 3
+const SUFFIX_LENGTH = 3
+
 export function shortenFilename(filename: string, length = 100): string {
   const name = path.parse(filename).name
   const extension = path.parse(filename).ext
@@ -7,7 +10,16 @@ export function shortenFilename(filename: string, length = 100): string {
   if (name.length < length) {
     return filename
   } else {
-    return shortenText(name, length) + '...' + extension
+    const shortened = shortenText(name, length)
+
+    if (shortened.length - SUFFIX_LENGTH >= MIN_PREFIX) {
+      const prefix = shortened.substr(0, shortened.length - SUFFIX_LENGTH)
+      const suffix = name.substr(name.length - SUFFIX_LENGTH, name.length)
+      
+      return prefix + '...' + suffix + extension
+    } else {
+      return shortened + '...' + extension
+    }
   }
 }
 
--- test/common/helpers/textTest.ts
+++ test/common/helpers/textTest.ts
@@ -5,8 +5,13 @@ describe('text', () => {
 
   describe('shortenFilename', () => {
     it('should shorten a filename to 10 characters without loosing the extension', () => {
-      const result = shortenFilename('bookmarks-2016-04-01bookmarks-2016-04-01bookmarks-2016-04-01bookmarks-2016-04-01bookmarks.json', 10)
-      expect(result).to.equal('bookmarks-....json')
+      const result = shortenFilename('bookmarks-2016-04-01bookmarks-2016-04-01bookmarks-2016-04-01bookmarks-2016-04-01bookmafoo.json', 10)
+      expect(result).to.equal('bookmar...foo.json')
+    })
+
+    it('should not use "Mac shortening" when text is too small', () => {
+      const result = shortenFilename('bookmarks.doc', 5)
+      expect(result).to.equal('bookm....doc')
     })
 
     it('should not shorten a filename that is less than the given length', () => {
