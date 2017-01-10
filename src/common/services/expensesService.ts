0e061ca1b940674090442482834ef5c86d98599c
Michael Mueller <haimich@gmx.net>
2017-01-10 23:52:44 +0100

    added expenses service

---
+++ src/common/services/expensesService.ts
@@ -0,0 +1,31 @@
+import ExpenseDbModel from '../models/ExpenseDbModel'
+import ExpenseModel from '../models/ExpenseModel'
+import * as expensesRepo from '../repositories/expensesRepository'
+
+export function expenseExists(id: number): Promise<boolean> {
+  return expensesRepo.expenseExists(id)
+}
+
+export function createExpense(expense: ExpenseModel): Promise<ExpenseDbModel> {
+  return expensesRepo.createExpense(expense)
+}
+
+export function updateExpense(expense: ExpenseDbModel): Promise<ExpenseDbModel> {
+  return expensesRepo.updateExpense(expense)
+}
+
+export function getExpenseById(id: number): Promise<ExpenseDbModel> {
+  return expensesRepo.getExpenseById(id)
+}
+
+export function listExpenses(): Promise<ExpenseDbModel[]> {
+  return expensesRepo.listExpenses()
+}
+
+export function deleteExpenseById(id: number): Promise<any> {
+  return expensesRepo.deleteExpenseById(id)
+}
+
+export function deleteExpensesByTypePattern(typePattern: string): Promise<any> {
+  return expensesRepo.deleteExpensesByTypePattern(typePattern)
+}
