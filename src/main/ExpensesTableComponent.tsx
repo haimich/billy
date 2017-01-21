1a29c02bce47a50ff5107b8b443a8ffe54571619
Michael Mueller <haimich@gmx.net>
2017-01-21 19:44:26 +0100

    fixed https://github.com/haimich/billy/issues/99

--- src/main/ExpensesTableComponent.tsx
+++ src/main/ExpensesTableComponent.tsx
@@ -119,11 +119,11 @@ export default class ExpensesTableComponent extends React.Component<Props, State
           <TableHeaderColumn isKey={true} hidden={true} dataField="id" width="140" dataSort={true}>{t('ID')}</TableHeaderColumn>
           <TableHeaderColumn dataField="type_name" width="150" dataSort={true}>{t('Typ')}</TableHeaderColumn>
           <TableHeaderColumn dataField="comment" width="350" dataSort={true}>{t('Kommentar')}</TableHeaderColumn>
-          <TableHeaderColumn dataField="preTaxAmount" width="70" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Brutto')}</TableHeaderColumn>
-          <TableHeaderColumn dataField="netAmount" width="70" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Netto')}</TableHeaderColumn>
-          <TableHeaderColumn dataField="vatAmount" width="70" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Mehrwertsteuer')}</TableHeaderColumn>
-          <TableHeaderColumn dataField="taxrate" width="70" dataAlign="right" dataFormat={this.formatTaxrate.bind(this)} dataSort={true}>{t('Steuersatz')}</TableHeaderColumn>
-          <TableHeaderColumn dataField="date" width="170" dataFormat={dateFormatterView} dataSort={true}>{t('Datum')}</TableHeaderColumn>
+          <TableHeaderColumn dataField="preTaxAmount" width="80" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Brutto')}</TableHeaderColumn>
+          <TableHeaderColumn dataField="netAmount" width="80" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Netto')}</TableHeaderColumn>
+          <TableHeaderColumn dataField="vatAmount" width="80" dataAlign="right" dataFormat={currencyFormatter} dataSort={true}>{t('Mwst.')}</TableHeaderColumn>
+          <TableHeaderColumn dataField="taxrate" width="80" dataAlign="right" dataFormat={this.formatTaxrate.bind(this)} dataSort={true}>{t('Steuersatz')}</TableHeaderColumn>
+          <TableHeaderColumn dataField="date" width="130" dataFormat={dateFormatterView} dataSort={true}>{t('Datum')}</TableHeaderColumn>
 
         </BootstrapTable>
       </div>
