cb8692287a5ee26a7d63423101921fe87d2a5fbb
Michael Mueller <haimich@gmx.net>
2017-01-29 18:50:22 +0100

    added migration step to change file paths to new location

--- sql/migrations/20170126193756_expense_files.js
+++ sql/migrations/20170126193756_expense_files.js
@@ -9,9 +9,24 @@ exports.up = (knex, Promise) => {
         table.unique(['expense_id', 'path'])
       })
     })
+    .then(() => {
+      console.log('Move bill file paths to new location')
+
+      return knex('bill_files').select('*')
+    })
+    .then((billFiles) => {
+      return Promise.all(
+        billFiles.map(fixPath)
+      )
+    })
 }
 
 exports.down = (knex, Promise) => {
   return knex.schema.renameTable('bill_files', 'files')
     .then(() => knex.schema.dropTableIfExists('expense_files'))
+}
+
+function fixPath(bill) {
+  let newPath = bill.path.replace('/billy/files/', '/billy/files/bills/')
+  return 
 }
