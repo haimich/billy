3e48770f2a6401314fcd64043977a787e1dabd11
Michael Müller <haimich@gmx.net>
2017-04-01 19:23:19 +0200

    fixed migration order

--- sql/migrations/20170202210946_bill_items.js
+++ sql/migrations/20170202210946_bill_items.js
@@ -1,26 +1,36 @@
-exports.up = (knex, Promise) => {
-  return knex.schema.createTable('bill_items', (table) => {
-    table.increments('id').primary().notNullable()
-    table.integer('bill_id').notNullable().references('id').inTable('bills')
-    table.integer('position').notNullable()
-    table.decimal('preTaxAmount').notNullable()
-    table.decimal('taxrate').notNullable()
-    table.text('description')
+let allBills
 
-    table.index('bill_id')
-  })
-    .then(() => {
-      return knex('bills').select('*')
-    })
+exports.up = (knex, Promise) => {
+  return knex('bills').select('*')
     .then(bills => {
-      return Promise.all(
-        bills.map(createBillArticle)
-      )
+      allBills = bills
+
+      return knex.schema.createTable('bill_items', (table) => {
+        table.increments('id').primary().notNullable()
+        table.integer('bill_id').notNullable().references('id').inTable('bills')
+        table.integer('position').notNullable()
+        table.decimal('preTaxAmount').notNullable()
+        table.decimal('taxrate').notNullable()
+        table.text('description')
+
+        table.index('bill_id')
+      })
     })
     .then(() => {
+      // this is necessary BEFORE creating the bill items because knex
+      // drops the bills table and then recreates it (foreign key problem).
+      console.log('Dropping "amount" column in bills')
+
       return knex.schema.table('bills', table => {
         table.dropColumn('amount')
       })
+    })
+    .then(() => {
+      console.log('Migrating ' + allBills.length + ' bills to items')
+
+      return Promise.all(
+        allBills.map(bill => createBillArticle(knex, bill))
+      )
     })
 }
 
@@ -28,7 +38,7 @@ exports.down = (knex, Promise) => {
   return knex.schema.dropTableIfExists('bill_items')
 }
 
-function createBillArticle(bill) {
+function createBillArticle(knex, bill) {
   return knex('bill_items')
     .insert({
       bill_id: bill.id,
--- sql/migrations/20170323200721_expense_items.js
+++ sql/migrations/20170323200721_expense_items.js
@@ -1,27 +1,37 @@
-exports.up = (knex, Promise) => {
-  return knex.schema.createTable('expense_items', (table) => {
-    table.increments('id').primary().notNullable()
-    table.integer('expense_id').notNullable().references('id').inTable('expenses')
-    table.integer('position').notNullable()
-    table.decimal('preTaxAmount').notNullable()
-    table.decimal('taxrate').notNullable()
-    table.text('description')
+let allExpenses
 
-    table.index('expense_id')
-  })
-    .then(() => {
-      return knex('expenses').select('*')
-    })
+exports.up = (knex, Promise) => {
+  return knex('expenses').select('*')
     .then(expenses => {
-      return Promise.all(
-        expenses.map(createExpenseItem)
-      )
+      allExpenses = expenses
+
+      return knex.schema.createTable('expense_items', (table) => {
+        table.increments('id').primary().notNullable()
+        table.integer('expense_id').notNullable().references('id').inTable('expenses')
+        table.integer('position').notNullable()
+        table.decimal('preTaxAmount').notNullable()
+        table.decimal('taxrate').notNullable()
+        table.text('description')
+
+        table.index('expense_id')
+      })
     })
     .then(() => {
+      // this is necessary BEFORE creating the expense items because knex
+      // drops the expenses table and then recreates it (foreign key problem).
+      console.log('Dropping "preTaxAmount" and "taxrate" columns in expenses')
+
       return knex.schema.table('expenses', table => {
         table.dropColumn('preTaxAmount')
         table.dropColumn('taxrate')
       })
+    })
+    .then(() => {
+      console.log('Migrating ' + allExpenses.length + ' expenses to items')
+
+      return Promise.all(
+        allExpenses.map(expense => createExpenseItem(knex, expense))
+      )
     })
 }
 
@@ -29,11 +39,11 @@ exports.down = (knex, Promise) => {
   return knex.schema.dropTableIfExists('expense_items')
 }
 
-function createExpenseItem(expense) {
+function createExpenseItem(knex, expense) {
   return knex('expense_items')
     .insert({
       expense_id: expense.id,
-      position: expense.position,
+      position: 0,
       preTaxAmount: expense.preTaxAmount,
       taxrate: expense.taxrate,
       description: ''
